4ae9f84ff5b7e8b5f62ea9049634834d2209068f
Martin is a hack <pathetic@human.com>
2014-12-20 06:25:53 -0800

    Updates
    hopefully fixing cors issue on twitch login

--- app.js
+++ app.js
@@ -101,6 +101,13 @@ if (cluster.isMaster) {
         })
     }));
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://"+ config.appAddress +":" + config.socketPort);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+        next();
+    });
+    
     passport.serializeUser(function(user, done) {
       done(null, user.id);
     });
