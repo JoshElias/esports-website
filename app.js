f112ff1039eedb3bccf22b14fc1e6183dbbf66b1
Martin is a hack <pathetic@human.com>
2015-05-15 09:59:51 -0700

    Updates
    Switching prerender to nginx.

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ if (cluster.isMaster) {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'dust');
 
-    app.use(require('prerender-node').set('prerenderToken', 'XrpCoT3t8wTNledN5pLU'));
+    //app.use(require('prerender-node').set('prerenderToken', 'XrpCoT3t8wTNledN5pLU'));
     app.use(favicon(path.join(__dirname, 'favicon.ico')));
     app.use(compression({
         threshold: 512
