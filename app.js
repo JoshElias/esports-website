bf882ed4893e5eb05096c8b80121ae15c3f62c23
Martin is a hack <pathetic@human.com>
2015-05-20 09:56:15 -0700

    Updates
    Removing poll from master

--- app.js
+++ app.js
@@ -174,8 +174,6 @@ if (cluster.isMaster) {
 
     app.post('/upload', routes.frontend.uploadToImgur(fs, imgur));
     
-    app.post('/poll', routes.frontend.poll(Schemas));
-
     /* frontend - requires login */
     app.post('/api/verify', routes.frontend.verify(Schemas));
 
