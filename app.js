67c82ffb979bce4f37886aa55fe624c16bb9260a
EC2 Default User <ec2-user@ip-172-31-38-138.us-west-2.compute.internal>
2015-06-19 16:27:46 +0000

    Tempostorm is Josh Elias

--- app.js
+++ app.js
@@ -46,8 +46,17 @@ var cluster = require('cluster'),
     twitter = require("./lib/twitter")
     assets = require("./lib/assets");
 
-/* mongoose */
-mongoose.connect(config.DB_URL);
+
+// mongoose 
+var replOptions = { 
+    replSet: {
+        rs_name: "tempostormRepSet",
+        readPreference: 'ReadPreference.NEAREST'
+    } 
+};
+var replSetUrl = "mongodb://52.26.156.55:27017,52.11.14.8:27017,52.8.169.246:27017/tempostorm";
+
+mongoose.connect(replSetUrl, replOptions);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -80,7 +89,7 @@ app.use(session({
     cookie: { expires: new Date(Date.now() + (60 * 60 * 24 * 7 * 1000)) },
     secret: config.SESSION_SECRET,
     store: new MongoStore({
-        url: config.DB_URL
+        mongooseConnection:  mongoose.connection
     })
 }));
 
@@ -108,7 +117,7 @@ passport.use(new BnetStrategy({
   }, routes.frontend.bnet(Schemas) ));
 
 
-/* twitch */
+// twitch 
 app.get('/auth/twitch', passport.authenticate('twitch'));
 app.get('/auth/twitch/callback', passport.authenticate('twitch', { failureRedirect: '/login' }),
   function(req, res) {
@@ -117,7 +126,7 @@ app.get('/auth/twitch/callback', passport.authenticate('twitch', { failureRedire
     res.redirect('/');
 });
 
-/* bnet */
+// bnet 
 app.get('/auth/bnet', passport.authenticate('bnet'));
 app.get('/auth/bnet/callback', passport.authenticate('bnet', { failureRedirect: '/login' }),
   function(req, res) {
@@ -128,11 +137,11 @@ app.get('/auth/bnet/callback', passport.authenticate('bnet', { failureRedirect: 
 
 
 
-/* spa */
+// spa 
 app.get('/', routes.frontend.index(config, assets));
 app.get('*', routes.frontend.index(config, assets));
 
-/* frontend */
+// frontend
 app.post('/login', routes.frontend.login(Schemas, jwt, config.JWT_SECRET));
 app.post('/signup', routes.frontend.signup(Schemas, uuid, Mail));
 app.post('/verify', routes.frontend.verifyEmail(Schemas, Mail, jwt, config.JWT_SECRET));
@@ -181,7 +190,7 @@ app.post('/polls/vote', routes.frontend.pollsVote(Schemas));
 
 app.post('/upload', routes.frontend.uploadToImgur(fs, imgur));
 
-/* frontend - requires login */
+// frontend - requires login 
 app.post('/api/verify', routes.frontend.verify(Schemas));
 
 app.post('/api/article/vote', routes.frontend.articleVote(Schemas));
@@ -219,7 +228,7 @@ app.post('/api/subscription/cancel', routes.frontend.subCancel(Schemas, Subscrip
 
 app.post('/api/contact/send', routes.frontend.sendContact(Mail));
 
-/* admin */
+// admin 
 app.post('/api/admin/cards', routes.admin.isAdmin(Schemas), routes.admin.cards(Schemas));
 app.post('/api/admin/card', routes.admin.isAdmin(Schemas), routes.admin.card(Schemas));
 app.post('/api/admin/card/add', routes.admin.isAdmin(Schemas), routes.admin.cardAdd(Schemas));
@@ -303,7 +312,7 @@ app.use(function (req, res, next) {
     return;
 });
 
-/* server start */
+// server start 
 var server = http.createServer(app);
 server.listen(config.APP_PORT);
-console.log('Starting server on port: '+config.APP_PORT);
+console.log('Starting server: '+config.APP_URL+":"+config.APP_PORT);
