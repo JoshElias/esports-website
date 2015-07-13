3c356379340e4d16df29fd9fe17149cf19d1d9f0
Darren B <darren.fake.email>
2015-07-13 10:02:41 -0700

    Changed og image on articles to large version

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3583,7 +3583,7 @@ angular.module('app.controllers', ['ngCookies'])
         $scope.metaservice = MetaService;
         $scope.metaservice.set($scope.article.title + ' - Articles', $scope.article.description);
         
-        var ogImg = $scope.app.cdn + 'articles/' + $scope.article.photos.small;
+        var ogImg = $scope.app.cdn + 'articles/' + $scope.article.photos.large;
         $scope.metaservice.setOg('https://tempostorm.com/articles/' + data.article.slug.url, $scope.article.title, $scope.article.description, 'article', ogImg);
         
         $scope.getContent = function () {
