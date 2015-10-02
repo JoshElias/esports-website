307049a1300fb7ee4823562dcbb512ae4ea24a83
Darren B <darren.fake.email>
2015-10-02 14:26:11 -0700

    development configured

--- server/boot/06-angular-root.js
+++ server/boot/06-angular-root.js
@@ -1,11 +1,10 @@
-var config = require("./../../common/config");
 var assets = require("./../../common/assets");
 
 
 module.exports = function(server) {
 
  	function indexHandler(req, res, next) {
- 		res.render(config.APP_INDEX, { cdnUrl: config.CDN_URL, assets: assets });
+ 		res.render(server.get("appIndex"), { cdnUrl: server.get("cdnUrl"), assets: assets });
  	}
 
 	server.get("*", indexHandler);
