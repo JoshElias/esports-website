1cc7478724caad5f6e46e5e74b2467489e10f20c
Martin is a hack <pathetic@human.com>
2015-10-08 13:45:47 -0700

    adding compass to grunt

---
+++ grunt/compass.js
@@ -0,0 +1,10 @@
+module.exports = {
+    build: {
+        options: {
+            outputStyle: 'expanded',
+            sassDir: 'client/css/sass',
+            cssDir: 'client/dist/css/sass',
+            cacheDir: 'client/css/.sass-cache',
+        }
+    }
+};
