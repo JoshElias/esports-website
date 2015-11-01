410ef5b47e01ce74bd387a3b9756f9b7e924d809
EC2 Default User <ec2-user@52.26.75.137>
2015-11-01 19:20:24 +0000

    added orderNum to OW Hero

--- common/models/overwatch-hero.js
+++ common/models/overwatch-hero.js
@@ -1,3 +1,26 @@
 module.exports = function(OverwatchHero) {
 
+
+    OverwatchHero.observe("before save", function(ctx, next) {
+
+        assignOrderNum(ctx.data, next);
+    });
+
+    function assignOrderNum(data, finalCallback) {
+        if(!data.id) finalCallback();
+
+        // If the hero already exists, it already has an orderNum
+        OverwatchHero.exists(ctx.data.id, function(err, exists) {
+            if(err) return finalCallback(err);
+            else if(exists) return finalCallback();
+
+            // Assign the orderNum to the current count of heroes
+            OverwatchHero.count(function(err, count) {
+                if(err) return finalCallback(err);
+
+                ctx.data.orderNum = count;
+                finalCallback();
+            });
+        });
+    }
 };
