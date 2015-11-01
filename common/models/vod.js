0246d22639c9d19bfb6c181946976953747a215a
EC2 Default User <ec2-user@52.26.75.137>
2015-11-01 21:35:27 +0000

    fixed registration bug. Added strict to models

--- common/models/vod.js
+++ common/models/vod.js
@@ -1,3 +1,8 @@
 module.exports = function(Vod) {
+    var utils = require("../../lib/utils");
 
+
+    Vod.observe("before save", function(ctx, next) {
+        utils.validateYoutubeId(ctx.data, next);
+    });
 };
