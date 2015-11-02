9d30ac452f5d4467d4700d59a92abda1aecf6ce1
Martin is a hack <pathetic@human.com>
2015-11-01 16:56:31 -0800

    fixing polls resolves

--- client/js/app.js
+++ client/js/app.js
@@ -1851,17 +1851,6 @@ var app = angular.module('app', [
                                         where: {
                                             'slug.url': post
                                         },
-                                        fields: {
-                                            id: true,
-                                            slug: true,
-                                            title: true,
-                                            authorId: true,
-                                            forumThreadId: true,
-                                            comments: true,
-                                            content: true,
-                                            viewCount: true,
-                                            createdDate: true
-                                        },
                                         include: [
                                             {
                                                 relation: 'forumThread',
@@ -1963,17 +1952,23 @@ var app = angular.module('app', [
                         templateUrl: tpl + 'views/frontend/polls.html',
                         controller: 'PollsCtrl',
                         resolve: {
-//                            dataPollsMain: ['PollService', function(PollService) {
-//                                return PollService.getPolls('main');
-//                            }],
                           dataPollsMain: ['Poll', function (Poll) {
-                            return Poll.find({}).$promise;
+                            return Poll.find({
+                                filter: {
+                                    where: {
+                                        view: 'main'
+                                    }
+                                }
+                            }).$promise;
                           }],
-//                            dataPollsSide: ['PollService', function(PollService) {
-//                                return PollService.getPolls('side');
-//                            }]
                           dataPollsSide: ['Poll', function (Poll) {
-                            return Poll.find({}).$promise;
+                            return Poll.find({
+                                filter: {
+                                    where: {
+                                        view: 'side'
+                                    }
+                                }
+                            }).$promise;
                           }]
                         }
                     }
