29503bbfc7296a6c68a94455688fc93bb129f22f
Martin is a hack <pathetic@human.com>
2015-11-18 17:16:26 -0800

    fixing map rows

--- public/js/services.js
+++ public/js/services.js
@@ -1200,7 +1200,7 @@ angular.module('app.services', [])
     hots.manaTypes = ['Mana', 'Brew', 'Energy', 'Fury'];
     hots.tiers = [1,4,7,10,13,16,20];
     hots.heroRows = [7, 8, 9, 8, 7, 6];
-    hots.mapRows = [5,4];
+    hots.mapRows = [3, 4, 3];
     
     hots.genStats = function () {
         var stats = [],
