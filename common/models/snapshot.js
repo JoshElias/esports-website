aa80c7ad3f3ae51d9d57251db294b7c9f77454c3
Josh Elias <code.5jzcm@simplelogin.com>
2015-12-11 16:04:00 -0800

    finally merged

--- common/models/snapshot.js
+++ common/models/snapshot.js
@@ -1,11 +1,3 @@
 module.exports = function(Snapshot) {
-    var utils = require("../../lib/utils");
 
-
-    var childrenNames = ["deckMatchups", "comments", "deckTiers", "authors"];
-    //Snapshot.observe("after save", utils.saveChildren(childrenNames));
-
-
-    var relationsToDestroy = ["deckMatchups", "comments", "deckTiers", "authors"];
-    //Snapshot.observe('before delete', utils.destroyRelations(relationsToDestroy));
 };
