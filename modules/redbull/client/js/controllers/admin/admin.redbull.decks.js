c7b149b08f45195af9b0a2ec6f5daea8bbd49ad3
luncht1me <lunchtime@nxtlvl.gg>
2016-02-03 16:46:06 -0800

    redbull admin wip

--- modules/redbull/client/js/controllers/admin/admin.redbull.decks.js
+++ modules/redbull/client/js/controllers/admin/admin.redbull.decks.js
@@ -1,3 +1,51 @@
 angular.module('redbull.controllers')
-.controller('AdminRedbullDecksCtrl', ['$scope', 'Hearthstone', function ($scope, Hearthstone){
+.controller('AdminRedbullDecksCtrl', ['$scope', 'Hearthstone', 'officialDrafts', 'officialPlayers', 'draftPlayers',
+    'RedbullDraft', 'RedbullDeck',
+  function ($scope, Hearthstone, officialDrafts, officialPlayers, draftPlayers,
+            RedbullDraft, RedbullDeck){
+
+    // FOR CLARIFICATION:
+    // officialPlayers are players that have completed an official draft, and are found by that draft's authorId
+    // draftPlayers are ALL whitelisted players.
+
+    $scope.officialDrafts = officialDrafts;
+    $scope.officialPlayers = officialPlayers;
+    $scope.draftPlayers = draftPlayers.players;
+
+    console.log("officialPlayers:", $scope.officialPlayers);
+
+    var officialPlayerIds = [];
+    var draftPlayerIds = [];
+
+    _.forEach($scope.officialPlayers, function(player){
+      officialPlayerIds.push(player.id);
+      _.forEach(officialDrafts, function(draft){
+        if(player.id === draft.authorId){
+          var extendDraft = {
+            draft:_.clone(JSON.parse(JSON.stringify(draft)))
+          };
+          _.extend(player, extendDraft);
+
+          console.log(extendDraft.draft);
+          console.log("extended player:", player);
+        }
+      })
+    });
+    _.forEach(draftPlayers.players, function(player){
+      draftPlayerIds.push(player.id);
+    });
+
+    var pendingPlayerIds =  _.difference(draftPlayerIds, officialPlayerIds);
+
+    $scope.pendingPlayers = _.filter($scope.draftPlayers, function(player){
+      return !(officialPlayerIds.includes(player.id))
+    });
+
+    console.log("PENDING PLAYERS", $scope.pendingPlayers);
+
+    console.log("officialDrafts", $scope.officialDrafts);
+
+
+
+
 }]);
