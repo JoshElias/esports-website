5b95012caa267ee116afc2c66cf17e7f334fcbf4
Darren B <darren.fake.email>
2016-02-18 13:29:44 -0800

    adding flexible adSlots

--- modules/adsense/client/js/app.js
+++ modules/adsense/client/js/app.js
@@ -1,6 +1,17 @@
 angular.module('tsAdSense', [])
+.run(
+    ['$rootScope', '$window',
+        function ($rootScope, $window) {
+            $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
+                
+                
+                
+            });
+        }
+    ]
+)
 .value('moduleTpl', (tpl !== './') ? tpl + 'views/adsense/client/views/' : 'dist/views/adsense/client/views/')
-.controller('tsAdCtrl', ['$scope', '$state', '$window', 'User', 'EventService', function ($scope, $state, $window, User, EventService) {
+.controller('tsAdCtrl', ['$scope', '$state', '$window', 'User', 'EventService', '$timeout', function ($scope, $state, $window, User, EventService, $timeout) {
     var url = 'http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
     var isAlreadyLoaded = !!document.getElementById("adCode");
     var role = undefined;
@@ -9,7 +20,6 @@ angular.module('tsAdSense', [])
     $scope.adBlock = window.canShowAds;
     
     function checkPremium () {
-        
         if (User.isAuthenticated() && !role) {
             User.isInRoles({
                 uid: User.getCurrentId(),
@@ -24,7 +34,6 @@ angular.module('tsAdSense', [])
                 
                 if (!role) {
                     $scope.showAds = true;
-                    console.log(e);
                     return doLoadAds();
                 }
                 
@@ -47,16 +56,23 @@ angular.module('tsAdSense', [])
     }
     
     function doLoadAds () {
+        console.log($scope);
         $scope.adClient = $scope.adClient || "ca-pub-6273013980199815";
         $scope.adSlot = $scope.adSlot || "7575226683";
         $scope.theme = $state.theme || 'default';
         $scope.region = $state.current.name;
+        $scope.w = (!_.isUndefined($scope.w)) ? $scope.w + 'px' : '100%';
+        $scope.h = (!_.isUndefined($scope.h)) ? $scope.h + 'px' : '100%';
         var e = $(".ad");
         
-//        for (var i = 0; i < e.length; i++) {
-//            console.log(e[i]);
-//            $(e[i]).removeClass('hidden');
-//        }
+        
+        
+        $timeout(function () {
+            for (var i = 0; i < e.length; i++) {
+                $(e[i]).removeClass('hidden');
+            }
+        })
+        
         
         if (!isAlreadyLoaded) {
             var s = document.createElement('script');
@@ -94,14 +110,27 @@ angular.module('tsAdSense', [])
                             $window.adsbygoogle.push({});
                         } catch (e) {
                             adIter++;
+                            console.error(e.message);
+                            console.error("Retrying");
                             return pushAd();
                         }
                     }, 500);
+                } else {
+                    console.log('timed out');
                 }
             } 
             
             pushAd();
-        }]
+        }],
+        link: function (scope, el, attrs) {
+//            $(el[0]).attr();
+            $timeout(function () {
+                console.log(attrs);
+                if (attrs.adSlot === "7575226683") {
+                    $(el[0]).attr('data-ad-format', 'auto')
+                }
+            })
+        }
     }
 }])
 .directive('tsAd', ['moduleTpl', '$compile', '$timeout', '$window', function (moduleTpl, $compile, $timeout, $window) {
@@ -114,7 +143,9 @@ angular.module('tsAdSense', [])
             inlineStyle : '@',
             region: '@',
             structure: '@',
-            theme: '@'
+            theme: '@',
+            h: '@', //height
+            w: '@'  //width
         },
         templateUrl: function (scope, attrs) {
             var tmp = attrs.structure || attrs.theme;
