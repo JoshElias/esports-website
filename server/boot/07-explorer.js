4f8f9ff8d0f799c31a79fa8a93954626291dc61f
Josh Elias <code.5jzcm@simplelogin.com>
2016-02-22 16:42:14 -0800

    updated loopback explorer

--- server/boot/07-explorer.js
+++ server/boot/07-explorer.js
@@ -6,19 +6,19 @@ module.exports = function mountLoopBackExplorer(server) {
     
   var explorer;
   try {
-    explorer = require('loopback-explorer');
+    explorer = require('loopback-component-explorer');
   } catch(err) {
     console.log(
-      'Run `npm install loopback-explorer` to enable the LoopBack explorer'
+      'Run `npm install loopback-component-explorer` to enable the LoopBack explorer'
     );
     return;
   }
 
   var restApiRoot = server.get('restApiRoot');
 
-  var explorerApp = explorer(server, { basePath: restApiRoot });
+  var explorerApp = explorer.routes(server, {basePath: restApiRoot});
   server.use('/explorer', explorerApp);
-  server.once('started', function() {
+  server.once('started', function () {
     var baseUrl = server.get('url').replace(/\/$/, '');
     // express 4.x (loopback 2.x) uses `mountpath`
     // express 3.x (loopback 1.x) uses `route`
