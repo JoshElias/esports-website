fe50ca940639b9db0eb5ab570ec8a568c991958b
Josh Elias <code.5jzcm@simplelogin.com>
2016-02-23 17:15:06 -0800

    fixed another explorer bug

--- server/boot/07-explorer.js
+++ server/boot/07-explorer.js
@@ -6,10 +6,10 @@ module.exports = function mountLoopBackExplorer(server) {
     
   var explorer;
   try {
-    explorer = require('loopback-explorer');
+    explorer = require('loopback-component-explorer');
   } catch(err) {
     console.log(
-      'Run `npm install loopback-explorer` to enable the LoopBack explorer'
+      'Run `npm install loopback-component-explorer` to enable the LoopBack explorer'
     );
     return;
   }
@@ -22,7 +22,7 @@ module.exports = function mountLoopBackExplorer(server) {
     var baseUrl = server.get('url').replace(/\/$/, '');
     // express 4.x (loopback 2.x) uses `mountpath`
     // express 3.x (loopback 1.x) uses `route`
-    var explorerPath = explorerApp.mountpath || explorerApp.route;
-    console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    //var explorerPath = explorerApp.mountpath || explorerApp.route;
+    console.log('Browse your REST API at %s', baseUrl);
   });
 };
