cc11992f1c9b3f1285667b6fce539b344f102077
Josh Elias <code.5jzcm@simplelogin.com>
2016-04-18 13:43:15 -0700

    testing twitch login

--- common/models/user-identity.js
+++ common/models/user-identity.js
@@ -1,178 +1,196 @@
 var loopback = require('loopback');
 
+
 module.exports = function(UserIdentity) {
 
 
-/**
-   * Log in with a third-party provider such as Facebook or Google.
-   *
-   * @param {String} provider The provider name.
-   * @param {String} authScheme The authentication scheme.
-   * @param {Object} profile The profile.
-   * @param {Object} credentials The credentials.
-   * @param {Object} [options] The options.
-   * @callback {Function} cb The callback function.
-   * @param {Error|String} err The error object or string.
-   * @param {Object} user The user object.
-   * @param {Object} [info] The auth info object.
-   *
-   * -  identity: UserIdentity object
-   * -  accessToken: AccessToken object
-   */
-  UserIdentity.login = function (provider, authScheme, profile, credentials,
-                                 options, cb) {
-      options = options || {};
-      if (typeof options === 'function' && cb === undefined) {
-          cb = options;
-          options = {};
-      }
-      var autoLogin = options.autoLogin || options.autoLogin === undefined;
-      var provider = (profile.battletag) ? "bnet" : "twitch"; // extend if needed
+    /**
+     * Log in with a third-party provider such as Facebook or Google.
+     *
+     * @param {String} provider The provider name.
+     * @param {String} authScheme The authentication scheme.
+     * @param {Object} profile The profile.
+     * @param {Object} credentials The credentials.
+     * @param {Object} [options] The options.
+     * @callback {Function} cb The callback function.
+     * @param {Error|String} err The error object or string.
+     * @param {Object} user The user object.
+     * @param {Object} [info] The auth info object.
+     *
+     * -  identity: UserIdentity object
+     * -  accessToken: AccessToken object
+     */
+    UserIdentity.login = function (provider, authScheme, profile, credentials,
+                                   options, cb) {
+        console.log("provider", provider);
+        console.log("authScheme", authScheme);
+        console.log("profile", profile);
+        console.log("credentials", credentials);
+        console.loG("options", options);
+        options = options || {};
+        if (typeof options === 'function' && cb === undefined) {
+            cb = options;
+            options = {};
+        }
+
+        // Third party login is not available for development
+        if(typeof process.env.NODE_ENV !== "string" || process.env.NODE_ENV === "development") {
+            var thirdPartyLoginErr = new Error('Third party login is not available in development');
+            thirdPartyLoginErr.statusCode = 400;
+            thirdPartyLoginErr.code = 'UNABLE_TO_LOGIN';
+            return cb(thirdPartyLoginErr);
+        }
+
+        var autoLogin = options.autoLogin || options.autoLogin === undefined;
+        var provider = (profile.battletag) ? "bnet" : "twitch"; // extend if needed
+        var redirectUrl = "https://"+UserIdentity.app.get("domain");
+        console.log("redirectUrl", redirectUrl);
 
 
-      // Set the result if available
-      var loopbackContext = loopback.getCurrentContext();
-      if (loopbackContext && loopbackContext.active && loopbackContext.active.http){
-          loopbackContext.set("res", loopbackContext.active.http.res);
-      }
+        // Set the result if available
+        var loopbackContext = loopback.getCurrentContext();
+        if (loopbackContext && loopbackContext.active && loopbackContext.active.http){
+            loopbackContext.set("res", loopbackContext.active.http.res);
+        }
 
-      console.log("finding with where:", provider, profile.id);
-      UserIdentity.findOne({
-          where: {
-              provider: provider,
-              externalId: profile.id
-          }
-      }, function (err, identity) {
-          console.log("found userIdentity", err, identity);
-          if (err) {
+        console.log("finding with where:", provider, profile.id);
+        return UserIdentity.findOne({
+            where: {
+                provider: provider,
+                externalId: profile.id
+            }
+        }, function (err, identity) {
+            console.log("found userIdentity", err, identity);
+            if (err) {
 
-              var res;
-              if (loopbackContext) {
-                  res = loopbackContext.get("res");
-              }
+                var res;
+                if (loopbackContext) {
+                    res = loopbackContext.get("res");
+                }
 
-              if(res) {
-                  console.log("responding to res");
-                  res.cookie("thirdPartyError", "Please link your " + provider + " profile to an email account first.", {
-                      signed: true,
-                      maxAge: 1209600
-                  });
-                  return res.redirect("https://tempostorm.com");
-              }
+                if(res) {
+                    console.log("responding to res");
+                    res.cookie("thirdPartyError", "Please link your " + provider + " profile to an email account first.", {
+                        signed: true,
+                        maxAge: 1209600
+                    });
+                    return res.redirect(redirectUrl);
+                }
 
-              return cb(err);
-          } else if (identity) {
-              console.log("found identity")
-              identity.credentials = credentials;
-              return identity.updateAttributes({
-                  profile: profile,
-                  credentials: credentials, modified: new Date()
-              }, function (err, i) {
-                  // Find the user for the given identity
-                  return identity.user(function (err, user) {
-                      // Create access token if the autoLogin flag is set to true
-                      if (!err && user && autoLogin) {
-                          return (options.createAccessToken || createAccessToken)(user, function (err, token) {
-                              cb(err, user, identity, token);
-                          });
-                      }
-                      cb(err, user, identity);
-                  });
-              });
-          } else {
-              console.log("responding with no identity");
-              var res;
-              if (loopbackContext) {
-                  res = loopbackContext.get("res");
-              }
+                return cb(err);
+            } else if (identity) {
+                console.log("found identity")
+                identity.credentials = credentials;
+                return identity.updateAttributes({
+                    profile: profile,
+                    credentials: credentials, modified: new Date()
+                }, function (err, i) {
+                    // Find the user for the given identity
+                    return identity.user(function (err, user) {
+                        // Create access token if the autoLogin flag is set to true
+                        if (!err && user && autoLogin) {
+                            return (options.createAccessToken || createAccessToken)(user, function (err, token) {
+                                cb(err, user, identity, token);
+                            });
+                        }
+                        cb(err, user, identity);
+                    });
+                });
+            } else {
+                console.log("responding with no identity");
+                var res;
+                if (loopbackContext) {
+                    res = loopbackContext.get("res");
+                }
 
-              if(res) {
-                  res.cookie("thirdPartyError", "Please link your " + provider + " profile to an email account first.", {
-                      signed: true,
-                      maxAge: 1209600
-                  });
-                  return res.redirect("https://tempostorm.com");
-              }
+                if(res) {
+                    res.cookie("thirdPartyError", "Please link your " + provider + " profile to an email account first.", {
+                        signed: true,
+                        maxAge: 1209600
+                    });
+                    return res.redirect(redirectUrl);
+                }
 
-              return cb(undefined, new Error("You must link your profile first"));
-          }
-      });
-  };
+                return cb(undefined, new Error("You must link your profile first"));
+            }
+        });
+    };
 
 
-	/**
-   * Link a third party account to a LoopBack user
-   * @param {String} provider The provider name
-   * @param {String} authScheme The authentication scheme
-   * @param {Object} profile The profile
-   * @param {Object} credentials The credentials
-   * @param {Object} [options] The options
-   * @callback {Function} cb The callback function
-   * @param {Error|String} err The error object or string
-   * @param {Object} [credential] The user credential object
-   */
-  UserIdentity.link = function (userId, provider, authScheme, profile,
+    /**
+     * Link a third party account to a LoopBack user
+     * @param {String} provider The provider name
+     * @param {String} authScheme The authentication scheme
+     * @param {Object} profile The profile
+     * @param {Object} credentials The credentials
+     * @param {Object} [options] The options
+     * @callback {Function} cb The callback function
+     * @param {Error|String} err The error object or string
+     * @param {Object} [credential] The user credential object
+     */
+    UserIdentity.link = function (userId, provider, authScheme, profile,
                                   credentials, options, cb) {
 
-    options = options || {};
-    if(typeof options === 'function' && cb === undefined) {
-      cb = options;
-      options = {};
+        options = options || {};
+        if(typeof options === 'function' && cb === undefined) {
+            cb = options;
+            options = {};
+        }
+
+        var provider = (profile.battletag) ? "bnet" : "twitch"; // extend if needed
+        UserIdentity.findOne({where: {
+            userId: userId,
+            provider: provider,
+            externalId: profile.id
+        }}, function (err, extCredential) {
+            if (err) {
+                return cb(err);
+            }
+
+            var date = new Date();
+            if (extCredential) {
+                // Find the user for the given extCredential
+                extCredential.credentials = credentials;
+                return extCredential.updateAttributes({profile: profile,
+                    credentials: credentials, modified: date}, cb);
+            }
+
+            // Create the linked account
+            var newUserIdentity = {
+                provider: provider,
+                externalId: profile.id,
+                authScheme: authScheme,
+                profile: profile,
+                credentials: credentials,
+                userId: userId,
+                created: date,
+                modified: date
+            };
+            console.log("new userIden", newUserIdentity);
+            return UserIdentity.create(newUserIdentity, function (err, i) {
+                cb(err, i);
+            });
+        });
     }
 
-	  var provider = (profile.battletag) ? "bnet" : "twitch"; // extend if needed
-    UserIdentity.findOne({where: {
-      userId: userId,
-      provider: provider,
-      externalId: profile.id
-    }}, function (err, extCredential) {
-      if (err) {
-        return cb(err);
-      }
 
-      var date = new Date();
-      if (extCredential) {
-        // Find the user for the given extCredential
-        extCredential.credentials = credentials;
-        return extCredential.updateAttributes({profile: profile,
-          credentials: credentials, modified: date}, cb);
-      }
-
-      // Create the linked account
-      UserIdentity.create({
-        provider: provider,
-        externalId: profile.id,
-        authScheme: authScheme,
-        profile: profile,
-        credentials: credentials,
-        userId: userId,
-        created: date,
-        modified: date
-      }, function (err, i) {
-        cb(err, i);
-      });
-
-    });
-  }
-
-
-	/*!
-	 * Create an access token for the given user
-	 * @param {User} user The user instance
-	 * @param {Number} [ttl] The ttl in millisenconds
-	 * @callback {Function} cb The callback function
-	 * @param {Error|String} err The error object
-		* param {AccessToken} The access token
-	 */
-	function createAccessToken(user, ttl, cb) {
-		if (arguments.length === 2 && typeof ttl === 'function') {
-			cb = ttl;
-			ttl = 0;
-		}
-		user.accessTokens.create({
-			created: new Date(),
-			ttl: Math.min(ttl || user.constructor.settings.ttl,
-				user.constructor.settings.maxTTL)
-		}, cb);
-	}
+    /*!
+     * Create an access token for the given user
+     * @param {User} user The user instance
+     * @param {Number} [ttl] The ttl in millisenconds
+     * @callback {Function} cb The callback function
+     * @param {Error|String} err The error object
+     * param {AccessToken} The access token
+     */
+    function createAccessToken(user, ttl, cb) {
+        if (arguments.length === 2 && typeof ttl === 'function') {
+            cb = ttl;
+            ttl = 0;
+        }
+        user.accessTokens.create({
+            created: new Date(),
+            ttl: Math.min(ttl || user.constructor.settings.ttl,
+                user.constructor.settings.maxTTL)
+        }, cb);
+    }
 };
